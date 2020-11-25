d07494a237f1caa1a133023cefbd3b6a60e331c6
Boldizsar Nemeth <boldizsar.nemeth@prezi.com>
2020-11-25 15:10:27 +0100

    Add retry-ability for the snapshot capabilities

--- src/command.js
+++ src/command.js
@@ -19,49 +19,61 @@ export function matchImageSnapshotCommand(defaultOptions) {
       ...((typeof maybeName === 'string' ? commandOptions : maybeName) || {}),
     };
 
-    cy.task(MATCH, {
-      screenshotsFolder,
-      updateSnapshots,
-      options,
-    });
+    const timeout = options.timeout || Cypress.config('defaultCommandTimeout');
 
-    const name = typeof maybeName === 'string' ? maybeName : undefined;
-    const target = subject ? cy.wrap(subject) : cy;
-    target.screenshot(name, options);
+    const startTime = Date.now();
 
-    return cy
-      .task(RECORD)
-      .then(
-        ({
-          pass,
-          added,
-          updated,
-          diffSize,
-          imageDimensions,
-          diffRatio,
-          diffPixelCount,
-          diffOutputPath,
-        }) => {
-          if (!pass && !added && !updated) {
-            const message = diffSize
-              ? `Image size (${imageDimensions.baselineWidth}x${
-                  imageDimensions.baselineHeight
-                }) different than saved snapshot size (${
-                  imageDimensions.receivedWidth
-                }x${
-                  imageDimensions.receivedHeight
-                }).\nSee diff for details: ${diffOutputPath}`
-              : `Image was ${diffRatio *
-                  100}% different from saved snapshot with ${diffPixelCount} different pixels.\nSee diff for details: ${diffOutputPath}`;
+    const match = () => {
+      cy.task(MATCH, {
+        screenshotsFolder,
+        updateSnapshots,
+        options,
+      });
 
-            if (failOnSnapshotDiff) {
-              throw new Error(message);
-            } else {
-              Cypress.log({ message });
+      const name = typeof maybeName === 'string' ? maybeName : undefined;
+      const target = subject ? cy.wrap(subject) : cy;
+      target.screenshot(name, options);
+
+      return cy
+        .task(RECORD)
+        .then(
+          ({
+            pass,
+            added,
+            updated,
+            diffSize,
+            imageDimensions,
+            diffRatio,
+            diffPixelCount,
+            diffOutputPath,
+          }) => {
+            if (!pass && !added && !updated) {
+              const message = diffSize
+                ? `Image size (${imageDimensions.baselineWidth}x${
+                    imageDimensions.baselineHeight
+                  }) different than saved snapshot size (${
+                    imageDimensions.receivedWidth
+                  }x${
+                    imageDimensions.receivedHeight
+                  }).\nSee diff for details: ${diffOutputPath}`
+                : `Image was ${diffRatio *
+                    100}% different from saved snapshot with ${diffPixelCount} different pixels.\nSee diff for details: ${diffOutputPath}`;
+
+              if (failOnSnapshotDiff) {
+                const currentTime = Date.now();
+                if (currentTime - startTime < timeout) {
+                  match();
+                } else {
+                  throw new Error(message);
+                }
+              } else {
+                Cypress.log(message);
+              }
             }
           }
-        }
-      );
+        );
+    };
+    match();
   };
 }
 
